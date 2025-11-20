c00d8ad05c5dc515206ad38b72f336c009431404
brucesilvasad-lang <brucesilva.sad@gmail.com>
2025-11-19 22:05:34 -0300

    Add files via upload

---
+++ geminiService.ts
@@ -0,0 +1,9 @@
+// IA DESATIVADA PARA EVITAR ERROS NO NAVEGADOR
+
+export const generateExercisePlan = async () => {
+  return "Recurso de IA temporariamente desativado.";
+};
+
+export const generateClassDescription = async () => {
+  return "Recurso de IA temporariamente desativado.";
+};
